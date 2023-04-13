76ce2480e4aa7a50855c1ddca5acfcd1c0d02837
StrangeresAnn <lykova_annushka@mail.ru>
2023-04-13 19:51:49 +0400

    Added_TASK7

--- src/notebook/log/impl/ConsoleLogger.java
+++ src/notebook/log/impl/ConsoleLogger.java
@@ -7,6 +7,6 @@ import java.time.LocalDateTime;
 public class ConsoleLogger implements Logger {
     @Override
     public void log(String text) {
-        System.err.println(LocalDateTime.now() + ": " + text);
+        System.err.println(LocalDateTime.now());
     }
 }
